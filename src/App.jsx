f6df716457fc103599a28f990d40e8a39b2f8020
AngeloParungao <angeloparungao.ap@gmail.com>
2024-08-30 13:35:34 +0800

    feat: splash

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { useMediaQuery } from "react-responsive";
 import Header from "./components/Header";
+import Splash from "./components/Splash";
 import "./App.css";
 import logos from "./assets/images";
 import profile from "./assets/profile.jpg";
@@ -14,11 +15,19 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
+  const [showSplash, setShowSplash] = useState(true);
   const [dark, setDarkMode] = useState(false);
 
   //--------------------------------------Screen Sizes--------------------------------------
   const isSmallScreen = useMediaQuery({ query: '(max-width: 1025px)' });
   
+  useEffect(() => {
+    setTimeout(() => {
+      setShowSplash(false);
+    }, 3000);
+  }, []);
+
+
   const toggleDarkMode = () => {
     setDarkMode((prevDark) => !prevDark);
   };
@@ -81,202 +90,215 @@ function App() {
       transition: { duration: 0.3, ease: "easeInOut" },
     }
   };
-  
+
 
   return (
     <div className={dark ? "dark" : ""}>
-      <Header dark={dark} toggleDarkMode={toggleDarkMode} />
-      {/*--------------------------------------Home Section--------------------------------------*/}
-      <section className="flex lg:flex-row flex-col-reverse justify-center items-center h-screen w-full px-10 lg:px-28 xl:px-35 lg:gap-0 gap-10">
-        <motion.div 
-          className="flex flex-col items-center lg:items-start w-full lg:w-1/2 gap-5"
-          initial={{ 
-            x: isSmallScreen ? "0" : "-3rem",
-            y: isSmallScreen ? "3rem" : "0",  
-            opacity: 0,
-          }}
-          whileInView={{
-            x: 0,
-            y: 0,
-            opacity: 1,
-          }}
-          transition={{
-            duration: 1,
-          }}
-        >
-          <span className="text-2xl md:text-5xl font-bold">
-            Angelo Miguel Parungao
-          </span>
-          <span className="text-xl md:text-3xl text-red-600">
-            Front-end Developer
-          </span>
-          <p className="font-medium text-gray-800 text-justify lg:pr-4 dark:text-gray-400 text-center lg:text-left">
-            I specialize in crafting responsive, user-friendly web interfaces
-            with a focus on clean, efficient code. Skilled in HTML, CSS,
-            JavaScript, and React, I bring designs to life with a commitment to
-            quality and performance. Explore my work to see how I turn ideas
-            into seamless digital experiences.
-          </p>
-          <div className="flex flex-row gap-4">
-            <button className="bg-transparent text-red-600 border border-2 border-red-600 py-2 px-4 rounded-xl hover:text-white hover:bg-red-600 transition duration-500">
-              Resume
-            </button>
-            <button className="bg-red-600 text-white border border-2 border-red-600 py-2 px-4 rounded-xl hover:bg-transparent hover:text-red-600 hover:dark:text-white transition duration-500">
-              Contact Me
-            </button>
-          </div>
+      {showSplash && 
+        <motion.div
+        initial={{ y: "0%" }}
+        animate={{ y: "-100%" }}
+        transition={{ duration: 1, delay: 2,ease: "easeInOut" }}
+      >
+          <Splash />
         </motion.div>
-        <div className="w-full lg:w-1/2 flex flex-col lg:flex-row justify-end items-center gap-4 lg:gap-20">
-          <motion.div 
-            className="outline outline-offset-8 outline-1 outline-black dark:outline-white animate-[wiggle_6s_ease-in-out_infinite]"
-            initial={{ 
-              x: isSmallScreen ? "0" : "3rem",
-              y: isSmallScreen ? "3rem" : "0",  
-              opacity: 0,
-            }}
-            whileInView={{
-              x: 0,
-              y: 0,
-              opacity: 1,
-            }}
-            transition={{
-              duration: 1,
-            }}
-          >
-            <img
-              src={profile}
-              alt="Angelo Miguel Parungao"
-              className={`w-60 lg:w-96 rounded-full shadow-lg outline outline-offset-8 outline-1 outline-black dark:outline-white animate-[wiggle_3s_ease-in-out_infinite]`}
-            />
-          </motion.div>
-          <div className="flex flex-row lg:flex-col justify-center items-center gap-8 text-xl">
-            <motion.a
-              href="https://www.facebook.com"
-              aria-label="Facebook"
-              initial={{ 
-                x: isSmallScreen ? "0" : "1rem",
-                y: isSmallScreen ? "1rem" : "0",  
-                opacity: 0,
-              }}
-              whileInView={{
-                x: 0,
-                y: 0,
-                opacity: 1,
-              }}
-              transition={{
-                duration: 1,
-              }}
-            >
-              <FontAwesomeIcon icon={faFacebook} />
-            </motion.a>
-            <motion.a
-              href="https://www.instagram.com"
-              aria-label="Instagram"
-              initial={{ 
-                x: isSmallScreen ? "0" : "1rem",
-                y: isSmallScreen ? "1rem" : "0",  
-                opacity: 0,
-              }}
-              whileInView={{
-                x: 0,
-                y: 0,
-                opacity: 1,
-              }}
-              transition={{
-                duration: 1,
-                delay: 0.5,
-              }}
-            >
-              <FontAwesomeIcon icon={faInstagram} />
-            </motion.a>
-            <motion.a
-              href="https://www.instagram.com"
-              aria-label="Instagram"
-              initial={{ 
-                x: isSmallScreen ? "0" : "1rem",
-                y: isSmallScreen ? "1rem" : "0",  
-                opacity: 0,
-              }}
-              whileInView={{
-                x: 0,
-                y: 0,
-                opacity: 1,
-              }}
-              transition={{
-                duration: 1,
-                delay: 1,
-              }}
-            >
-              <FontAwesomeIcon icon={faGithub} />
-            </motion.a>
-            <motion.a
-              href="https://www.linkedin.com"
-              aria-label="Linkedin"
-              initial={{ 
-                x: isSmallScreen ? "0" : "1rem",
-                y: isSmallScreen ? "1rem" : "0",  
-                opacity: 0,
-              }}
-              whileInView={{
-                x: 0,
-                y: 0,
-                opacity: 1,
-              }}
-              transition={{
-                duration: 1,
-                delay: 1.5,
-              }}
-            >
-              <FontAwesomeIcon icon={faLinkedin} />
-            </motion.a>
+      }
+      {!showSplash &&
+          <div>
+            <Header dark={dark} toggleDarkMode={toggleDarkMode} />
+            {/*--------------------------------------Home Section--------------------------------------*/}
+            <section className="flex lg:flex-row flex-col-reverse justify-center items-center h-screen w-full px-10 lg:px-28 xl:px-35 lg:gap-0 gap-10">
+              <motion.div 
+                className="flex flex-col items-center lg:items-start w-full lg:w-1/2 gap-5"
+                initial={{ 
+                  x: isSmallScreen ? "0" : "-3rem",
+                  y: isSmallScreen ? "3rem" : "0",  
+                  opacity: 0,
+                }}
+                whileInView={{
+                  x: 0,
+                  y: 0,
+                  opacity: 1,
+                }}
+                transition={{
+                  duration: 1,
+                }}
+              >
+                <span className="text-2xl md:text-5xl font-bold">
+                  Angelo Miguel Parungao
+                </span>
+                <span className="text-xl md:text-3xl text-red-600">
+                  Front-end Developer
+                </span>
+                <p className="font-medium text-gray-800 text-justify lg:pr-4 dark:text-gray-400 text-center lg:text-left">
+                  I specialize in crafting responsive, user-friendly web interfaces
+                  with a focus on clean, efficient code. Skilled in HTML, CSS,
+                  JavaScript, and React, I bring designs to life with a commitment to
+                  quality and performance. Explore my work to see how I turn ideas
+                  into seamless digital experiences.
+                </p>
+                <div className="flex flex-row gap-4">
+                  <button className="bg-transparent text-red-600 border border-2 border-red-600 py-2 px-4 rounded-xl hover:text-white hover:bg-red-600 transition duration-500">
+                    Resume
+                  </button>
+                  <button className="bg-red-600 text-white border border-2 border-red-600 py-2 px-4 rounded-xl hover:bg-transparent hover:text-red-600 hover:dark:text-white transition duration-500">
+                    Contact Me
+                  </button>
+                </div>
+              </motion.div>
+              <div className="w-full lg:w-1/2 flex flex-col lg:flex-row justify-end items-center gap-4 lg:gap-20">
+                <motion.div 
+                  className="outline outline-offset-8 outline-1 outline-black dark:outline-white animate-[wiggle_6s_ease-in-out_infinite]"
+                  initial={{ 
+                    x: isSmallScreen ? "0" : "3rem",
+                    y: isSmallScreen ? "3rem" : "0",  
+                    opacity: 0,
+                  }}
+                  whileInView={{
+                    x: 0,
+                    y: 0,
+                    opacity: 1,
+                  }}
+                  transition={{
+                    duration: 1,
+                  }}
+                >
+                  <img
+                    src={profile}
+                    alt="Angelo Miguel Parungao"
+                    className={`w-60 lg:w-96 rounded-full shadow-lg outline outline-offset-8 outline-1 outline-black dark:outline-white animate-[wiggle_3s_ease-in-out_infinite]`}
+                  />
+                </motion.div>
+                <div className="flex flex-row lg:flex-col justify-center items-center gap-8 text-xl">
+                  <motion.a
+                    href="https://www.facebook.com"
+                    aria-label="Facebook"
+                    initial={{ 
+                      x: isSmallScreen ? "0" : "1rem",
+                      y: isSmallScreen ? "1rem" : "0",  
+                      opacity: 0,
+                    }}
+                    whileInView={{
+                      x: 0,
+                      y: 0,
+                      opacity: 1,
+                    }}
+                    transition={{
+                      duration: 1,
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faFacebook} />
+                  </motion.a>
+                  <motion.a
+                    href="https://www.instagram.com"
+                    aria-label="Instagram"
+                    initial={{ 
+                      x: isSmallScreen ? "0" : "1rem",
+                      y: isSmallScreen ? "1rem" : "0",  
+                      opacity: 0,
+                    }}
+                    whileInView={{
+                      x: 0,
+                      y: 0,
+                      opacity: 1,
+                    }}
+                    transition={{
+                      duration: 1,
+                      delay: 0.5,
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faInstagram} />
+                  </motion.a>
+                  <motion.a
+                    href="https://www.instagram.com"
+                    aria-label="Instagram"
+                    initial={{ 
+                      x: isSmallScreen ? "0" : "1rem",
+                      y: isSmallScreen ? "1rem" : "0",  
+                      opacity: 0,
+                    }}
+                    whileInView={{
+                      x: 0,
+                      y: 0,
+                      opacity: 1,
+                    }}
+                    transition={{
+                      duration: 1,
+                      delay: 1,
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faGithub} />
+                  </motion.a>
+                  <motion.a
+                    href="https://www.linkedin.com"
+                    aria-label="Linkedin"
+                    initial={{ 
+                      x: isSmallScreen ? "0" : "1rem",
+                      y: isSmallScreen ? "1rem" : "0",  
+                      opacity: 0,
+                    }}
+                    whileInView={{
+                      x: 0,
+                      y: 0,
+                      opacity: 1,
+                    }}
+                    transition={{
+                      duration: 1,
+                      delay: 1.5,
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faLinkedin} />
+                  </motion.a>
+                </div>
+              </div>
+            </section>
+            {/*--------------------------------------About Section--------------------------------------*/}
+            <section className="h-screen flex flex-col justify-center items-center w-full px-10 lg:px-28 xl:px-35">
+              <div className="flex flex-col items-center w-full">
+                <motion.span className="w-full font-medium text-base lg:text-xl mb-4 border-b-2 border-dashed border-gray-400 dark:border-gray-600 pb-3"
+                  initial={{
+                    translateX: "-10%",
+                    opacity: 0,
+                  }}
+                  whileInView={{
+                    translateX: 0,
+                    opacity: 1,
+                  }}
+                  transition={{
+                    duration: 2,
+                  }}
+                >
+                  TECHNOLOGIES
+                </motion.span>
+                <div className="flex flex-wrap justify-center items-center gap-14 py-10">
+                  {
+                    srcs.map((src , index) => {
+                      return (
+                        <div className="flex flex-col justify-center items-center" key={src.title}>
+                          <motion.img
+                            src={src.logo} title={src.title}
+                            className="w-16 rounded-lg p-3 px-3 backdrop-blur-md cursor-pointer"
+                            initial={techAnimations.initial}
+                            animate={techAnimations.animate}
+                            whileHover={techAnimations.whileHover}
+                            transition={{
+                              duration: 5,
+                              repeat: Infinity,
+                              ease: "easeInOut",
+                              delay: index * 0.5,
+                            }}
+                            alt={src.title}
+                          />
+                          <span className="text-sm text-gray-600 font-medium">{src.title}</span>
+                        </div>
+                      )
+                    })
+                  }
+                </div>
+              </div>
+            </section>
           </div>
-        </div>
-      </section>
-      {/*--------------------------------------About Section--------------------------------------*/}
-      <section className="h-screen flex flex-col justify-center items-center w-full px-10 lg:px-28 xl:px-35">
-        <div className="flex flex-col items-center w-full">
-          <motion.span className="w-full font-medium text-base lg:text-xl mb-4 border-b-2 border-dashed border-gray-400 dark:border-gray-600 pb-3"
-            initial={{
-              translateX: "-10%",
-              opacity: 0,
-            }}
-            whileInView={{
-              translateX: 0,
-              opacity: 1,
-            }}
-            transition={{
-              duration: 2,
-            }}
-          >
-            TECHNOLOGIES
-          </motion.span>
-          <div className="flex flex-wrap justify-center items-center gap-14 py-10">
-            {
-              srcs.map((src , index) => {
-                return (
-                  <div className="flex flex-col justify-center items-center" key={src.title}>
-                    <motion.img
-                      src={src.logo} title={src.title}
-                      className="w-16 rounded-lg p-3 px-3 backdrop-blur-md cursor-pointer"
-                      initial={techAnimations.initial}
-                      animate={techAnimations.animate}
-                      whileHover={techAnimations.whileHover}
-                      transition={{
-                        duration: 5,
-                        repeat: Infinity,
-                        ease: "easeInOut",
-                        delay: index * 0.5,
-                      }}
-                      alt={src.title}
-                    />
-                    <span className="text-sm text-gray-600 font-medium">{src.title}</span>
-                  </div>
-                )
-              })
-            }
-          </div>
-        </div>
-      </section>
+        }
     </div>
   );
 }
---
+++ src/components/Splash.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Splash() {
+  return (
+    <div className='h-screen flex justify-center items-center dark'>
+      <span className='text-7xl font-bold'>HELLO.</span>
+    </div>
+  )
+}
+
+export default Splash
